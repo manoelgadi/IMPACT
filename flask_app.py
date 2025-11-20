5ebcb2ef8f0a32d2c9eddba943dca47a5e75179d
Prof. Manoel Gadi <mfalonso@faculty.ie.edu>
2025-11-20 13:14:37 +0100

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,50 @@
+from flask import Flask
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return """Hello from flask!"""
+
+from joblib import load
+model = load('fintech.joblib')
+
+@app.route("/probdefaultfixed")
+def probdefaultfixed():
+    """ Probability of Default Harded Coded """
+    # dictionary with list object in values
+    data = {
+        'time_on_books' : [36],
+        'total_paid_last_12_months' : [2000],
+        'total_debt_in_arrears' : [4000]
+    }
+
+    # creating a Dataframe object
+    data_df = pd.DataFrame(data)
+    X = data_df[['time_on_books', 'total_paid_last_12_months', 'total_debt_in_arrears']]
+
+    # predict
+    prob_default = model.predict_proba(X)[:,1]
+
+    return "Probability of default: {}".format(prob_default)
+
+
+@app.route("/probdefault")
+def probdefault():
+    """ Probability of receiving parameters from HTML using GET """
+
+    data = {
+        'time_on_books' : [request.args['time_on_books']],
+        'total_paid_last_12_months' : [request.args['total_paid_last_12_months']],
+        'total_debt_in_arrears' : [request.args['total_debt_in_arrears']]
+    }
+
+    # creating a Dataframe object
+    data_df = pd.DataFrame(data)
+    X = data_df[['time_on_books', 'total_paid_last_12_months', 'total_debt_in_arrears']]
+
+    # predict
+    prob_default = model.predict_proba(X)[:,1]
+
+    return "Probability of default: {}".format(prob_default)
